b08796ee633c343b4d32eebc1e091fab5ff1bd91
Felo <felipemelo713@gmail.com>
2023-04-20 13:43:46 -0300

    Atualização de cor por tipo de pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,30 @@ const maxRecords = 151
 const limit = 10
 let offset = 0;
 
+const typeColors = {
+  normal: "#A8A878",
+  fire: "#F08030",
+  water: "#6890F0",
+  electric: "#F8D030",
+  grass: "#78C850",
+  ice: "#98D8D8",
+  fighting: "#C03028",
+  poison: "#A040A0",
+  ground: "#E0C068",
+  flying: "#A890F0",
+  psychic: "#F85888",
+  bug: "#A8B820",
+  rock: "#B8A038",
+  ghost: "#705898",
+  dragon: "#7038F8",
+  dark: "#705848",
+  steel: "#B8B8D0",
+  fairy: "#EE99AC"
+};
+
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li class="pokemon ${pokemon.type}" style="background-color: ${typeColors[pokemon.types[0]]}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
